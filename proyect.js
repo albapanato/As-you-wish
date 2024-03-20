7bf035f7d3570e07c09ffce5bbdd5e8b27e74f99
Alba Panato Alegre <159134362+albapanato@users.noreply.github.com>
2024-03-20 08:40:35 +0100

    Add files via upload

---
+++ proyect.js
@@ -0,0 +1,191 @@
+const characters = [
+    {
+        name: 'Tanjiro Kamado',
+        race: 'Human',
+        affiliation: 'Kamado Family, Demon Slayer Corps',
+        skill: 'Water Breathing and Sun Breathing',
+        quote: "I'll carry the weight of sadness for those who've died.",
+        img: "svg/personajes-principales/tanjiro.webp",
+    },
+
+    {
+        name: 'Nezuko Kamado',
+        race: 'Demon',
+        affiliation: 'Kamado Family, Demon Slayer Corps',
+        skill: 'Blood Demon Art',
+        quote: "Humans are to be protected and saved. I will never hurt them.",
+        img: "C:/Users/44770/albachiara/Practicas/HTML,CSS Y JS/Proyecto-mio/svg/personajes-principales/nezuko.webp",
+    },
+
+    {
+        name: 'Zenitsu Agatsuma',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Thunder Breathing',
+        quote: "I can only do the first form, but you can do everything except that.",
+        img: "C:/Users/44770/albachiara/Practicas/HTML,CSS Y JS/Proyecto-mio/svg/personajes-principales/demon-slayer-2366105.webp",
+    },
+
+    {
+        name: 'Inosuke Hashibira',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Beast Breathing',
+        quote: "I'm not going to die! I'm going to survive even if it kills me!",
+        img: "svg/personajes-principales/inosuke-demon-slayer..jpg",
+    },
+
+    {
+        name: 'Muzan Kibutsuji',
+        race: 'Demon',
+        affiliation: 'Twelve Kizuki',
+        skill: 'Whip Proficiency, Biokinesis',
+        quote: "Does my complexion look unhealthy to you? Is my face pale? Do I look sickly to you? Do I look like I'm not long for this world? Do I look like I'm about to die? Wrong, wrong, wrong, wrong. I'm a living being who's infinitely close to perfection.",
+        img: "svg/demonios/Demon-Slayer-Episode-07-Figure-09.webp",
+    },
+
+    {
+        name: 'Kanao Tsuyuri',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Flower Breathing',
+        quote: "I don't want to regret the choices I make.",
+        img: "svg/personajes-secundarios/kanao_tsuyuri.webp",
+    },
+
+    {
+        name: 'Giyu Tomioka',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Water Breathing',
+        quote: "I want you to live your life to the fullest. Do your best and make your own decisions. And don't let others decide things for you.",
+        img: "svg/pilares/giyu-tomioka.jpg",
+    },
+
+    {
+        name: 'Mitsuri Kanroji',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Love Breathing',
+        quote: "You've gained even more scars! How gorgeous you look!",
+        img: "svg/pilares/mitsuri-kanroji.webp",
+    },
+
+    {
+        name: 'Obanai Iguro',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Serpent Breathing',
+        quote: "In the first place, I loathe demons.",
+        img: "svg/pilares/obani-iguro.jpg",
+    },
+
+
+    {
+        name: 'Sanemi Shinazugawa',
+        race: 'Human',
+        affiliation: 'Shinazugawa Family, Demon Slayer Corps',
+        skill: 'Wind Breathing',
+        quote: "Is that the moron Slayer who has a demon with him?",
+        img: "svg/pilares/sanemi-shinazugawa.jpg",
+    },
+
+    {
+        name: 'Gyomei Himejima',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Stone Breathing',
+        quote: "Let us kill this poor child immediately so that she can be liberated!",
+        img: "svg/pilares/gyomei-himejima.jpg",
+    },
+
+
+    {
+        name: 'Muichiro Tokito',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Mist Breathing',
+        quote: "Just like master said, if you recover your confidence... you can dig in with both feet and use your full strength. If you know who you are then no demon can run when you bring down a blade that knows no hesitation or confusion.",
+        img: "svg/pilares/muichiro-tokito.jpg",
+    },
+
+    {
+        name: 'Shinobu Kocho',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Insect Breathing',
+        quote: "How can you feel sorry for something that's killed humans? I've never heard of anything so absurd. But if that was how my sister truly felt, then I must carry it on. If there's a way not to kill these pitiful demons, I have to keep trying to come up with it. Without ever extinguishing the smile my sister said she loved..",
+        img: "svg/pilares/shinobu_kocho.webp",
+    },
+
+    {
+        name: 'Kyojuro Rengoku',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Flame Breathing',
+        quote: "Life is a series of decisions. You never have unlimited options or unlimited time to think, but what you choose in that instant defines who you are. Warriors who strive to save other people's lives are precious...",
+        img: "svg/pilares/kyojuru-rengoku.jpg",
+    },
+
+    {
+        name: 'Kanae Kocho',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'Flower Breathing',
+        quote: "Pull yourself together. I won't allow you to cry. Stand up.",
+        img: "svg/pilares/Kanae_Kochou_-_Anime.webp",
+
+    },
+
+    {
+        name: 'Kagaya Ubuyashiki',
+        race: 'Human',
+        affiliation: 'Demon Slayer Corps',
+        skill: 'none',
+        quote: "I know what eternity is. Eternity is human feeling. Only human feelings last forever and are undying.",
+        img: "svg/pilares/kagaya-ubuyashiki.jpg",
+    },
+
+
+]
+
+var characterNow = 0;
+
+function showData() {
+    var info = characters[characterNow];
+    const photo = info.img;
+    document.getElementById("name").textContent = " Name: " + info.name;
+    document.getElementById("race").textContent = " Race: " + info.race;
+    document.getElementById("skill").textContent = " Skill: " + info.skill;
+    document.getElementById("quote").textContent = " Quote: " + info.quote;
+    document.getElementById("photo").src = photo;
+}
+
+
+
+document.getElementById("buttonNext").addEventListener("click", function () {
+    characterNow++;
+    if (characterNow >= characters.length) {
+        characterNow = 0;
+
+    }
+    showData();
+
+});
+
+document.getElementById("buttonBack").addEventListener("click", function () {
+    characterNow--;
+    if (characterNow < 0) {
+        characterNow = characters.length - 1;
+
+    }
+    showData();
+});
+
+showData();
+
+var video = document.getElementById("video-background");
+video.playbackRate = 0.6;
+
+var video = document.getElementById("video-front-page");
+video.playbackRate = 0.3;
